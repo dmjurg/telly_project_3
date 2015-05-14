18a97f1bfc9ce117e5a9959fe8834b439ef92ed3
DIANA JURGIELEWICZ <DMJURG@GMAIL.COM>
2015-05-14 12:42:11 -0400

    display

--- app/assets/javascripts/telly.js
+++ app/assets/javascripts/telly.js
@@ -4,68 +4,32 @@ var showSearch = $(".showSearch")
 var showButton = $(".showButton")
 var url = "https://api.themoviedb.org/3/search/tv?api_key=059d376b9f7dcd72de6672af1113fcf1&query="
 
-// var Show = function(id, name, first_air_date, rating){
-//   this.id = id;
-//   this.name = name;
-//   this.first_air_date = first_air_date;
-//   this.rating = 0;
-// }
-//
-// showButton.on("click", function(){
-//   searchURL = showSearch.val().split(" ").join("+")
-// })
-//
-
-//
-// console.log(searchURL)
-//
-// searchModel = new Search()
-//
-// Search.prototype = {
-//
-//     fetchShows: function() {
-//       $.ajax({
-//         type: "get",
-//         dataType: "json",
-//         url: url + searchURL
-//       }).done(function( response ){
-//         searchModel.loadShows( response )
-//       }).fail(function( response ){
-//         console.log("fail")
-//       })
-//     },
-//     loadShows: function( response ){
-//       this.shows = [];
-//       for(var i = 0; i < response.length; i++){
-//         var show = new Show(response[i].id, response[i].name, response[i].first_air_date, 0);
-//         this.shows.push( show );
-//       }
-//     }
-//
-// }
-
-
-
 showButton.on("click", function(){
-  showSearch = showSearch.val().split(" ").join("+")
-  $.ajax({
-    url: url + showSearch,
-    type: "get",
-    dataType: "json"
-  }).done(function( response ){
-    searchResults = [];
-    for(var i = 0; i < response.results.length; i++){
-      var name = response.results[i].name
-        searchResults.push( name )
-      }
-    console.log( searchResults )  
-  }).fail(function(){
-    console.log("try again.  woof.")
-  })
+  var searchString = showSearch.val().split(" ").join("+");
+    $.ajax({
+      url: url + searchString,
+      type: "get",
+      dataType: "json"
+    }).done(function(response){
+      var searchResults = [];
+      for(var i = 0; i < response.results.length; i++){
+        var name = response.results[i].name
+          searchResults.push( name )
+        };
+      for(var i = 0; i < searchResults.length; i++){
+        var showOption = searchResults[i]
+        $(".show_results").append($('<input type="button" value="Add to favorites" id="fave">')).append(showOption)
+        }
+    }).fail(function(){
+      console.log("try again.  woof.")
+    });
 
 })
 
-
+$("#fave").on("click", function(){
+    console.log("FAVORITED.  BAM.")
+  })
+    
 
 })
 
